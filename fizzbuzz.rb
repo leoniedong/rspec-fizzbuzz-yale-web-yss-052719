6320758326bb25a7afca5645a5ae31eae6cbd8db
Leonie Dong <leoniedong@hotmail.com>
2019-03-11 09:45:11 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,14 @@
 def fizzbuzz(int)
   if int % 3 == 0
-    "Fizz"
+    puts "Fizz"
   end
 
   if int % 5 == 0
-    "Buzz"
+    puts "Buzz"
   end
 
   if int % 15 == 0
-    "FizzBuzz"
+    puts "FizzBuzz"
   end
 
 end
